1160a1a1bf7e5017f6dd83b19fcb98eac4329d41
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2023-03-25 19:45:27 +0100

    Fix book buttons

--- modules.js
+++ modules.js
@@ -1,18 +1,17 @@
 const getBookCardHTML = (book) => {
-  return `
+    return `
     <div class="col-xxl-2 col-xl-3 col-lg-3 col-md-4 col-sm-6">
      
     <div class="card card-img-top h-100">
         <img src="${book.image}" />
         <div class="card-body">
-            <h5 class="card-title"> Title: ${book.title} </h5>
-            <h6 class="card-author"> Author: ${book.author} </h6>
-            <h6 class="card-price"> Price: ${book.price} kr </h6>
-            <h6 class="card-category"> Category: ${book.category} </h6>
+            <h5 class="card-title"> ${book.title} </h5>
+            <h6 class="card-author"> ${book.author} </h6>
+            <h5 class="card-price"> ${book.price} SEK </h5>
         </div>
         <div class="card-body">
-            <a href="#" id="popDetail" class="card-link" data-bs-toggle="modal" data-bs-target="#popModal">Details</a>
-            <a href="#" id="purchase" class="card-link">Purchase</a>
+            <a href="#" id="purchase" class="card-link btn btn-primary">ADD TO CART</a>
+            <a href="#" id="popDetail" class="card-link btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#popModal">VIEW</a>            
         </div>
     </div>
     </div>
@@ -21,7 +20,7 @@ const getBookCardHTML = (book) => {
         <div class="modal-dialog">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Book title: ${book.title} </h5>
+                <h5 class="modal-title" id="exampleModalLabel"> ${book.title} </h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -35,8 +34,8 @@ const getBookCardHTML = (book) => {
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Purchase</button>
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">ADD TO CART</button>
+                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
             </div>
             </div>
         </div>
--- pages.js
+++ pages.js
@@ -3,7 +3,7 @@ import {getBookCardHTML} from './modules.js';
 
 let booksData = [];
 
-// Create a load method
+// Load the books cards
 async function loadContent() {
 
     // Get books data 
@@ -14,11 +14,12 @@ async function loadContent() {
         contentHtml += getBookCardHTML(book);
     })
 
+    // Add html content to the shop page structure
     document.querySelector("#books").innerHTML = contentHtml;
 }
 
+// Loads the Shop page structure
 function loadShopPage() {
-    // console.log("in main");
     document.querySelector("main").innerHTML = `
     <div class="container rounded m2-auto ">
       <div id="books" class="row text-center mt-2 g-3 flex-row justify-content-center">
